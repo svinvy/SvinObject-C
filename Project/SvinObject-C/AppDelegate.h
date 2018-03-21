e284fdcbbca1653ea7a32ce73ffd3101675c87af
SKY <guojianpeng@svinvy.com>
2018-03-21 09:39:57 +0800

    project init

---
+++ Project/SvinObject-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SvinObject-C
+//
+//  Created by GJP on 2018/3/21.
+//  Copyright © 2018年 svinvy.lnc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Project/SvinObject-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SvinObject-C
+//
+//  Created by GJP on 2018/3/21.
+//  Copyright © 2018年 svinvy.lnc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
